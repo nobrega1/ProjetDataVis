4fa4f21f73eaca58dd04cc603672fb99ce612a65
nobrega1 <56161445+nobrega1@users.noreply.github.com>
2020-04-24 16:18:17 +0200

    uptade

--- src/js/Games_per_year/graphGPY.js
+++ src/js/Games_per_year/graphGPY.js
@@ -1,57 +1,7 @@
-var chart = bb.generate({
-    data: {
-      x: "x",
-      columns: [
-      ["x", "2013-01-01", "2013-01-02", "2013-01-03", "2013-01-04", "2013-01-05", "2013-01-06"],
-      ["data1", [150, 140, 110],
-      [155, 130, 115],
-      [160, 135, 120],
-      [135, 120, 110],
-      [180, 150, 130],
-      [199, 160, 125]
-      ],
-      ["data2", 130, 340, 200, 500, 250, 350]
-      ],
-      types: {
-        data1: "area-line-range"
-      }
-    },
-    axis: {
-      x: {
-        type: "timeseries",
-        tick: {
-          format: "%Y-%m-%d"
-        }
-      }
-    },
-    bindto: "#areaRangeChart"
-  });
-  
-  setTimeout(function() {
-      chart.load({
-          columns: [
-              ["data3", [220, 215, 205], [240, 225, 215], [260, 235, 225], [280, 245, 235], [270, 255, 225], [240, 225, 215]],
-          ],
-          types: {
-              data3: "area-spline-range"
-          }
-      });
-  }, 1000);
-  
-  setTimeout(function() {
-      chart.load({
-          columns: [
-              ["data4",
-                  {high: 155, low: 145, mid: 150},
-                  {high: 200, mid: 190, low: 150},
-                  {high: 230, mid: 215, low: 200},
-                  {high: 210, mid: 200, low: 180},
-                  {high: 220, mid: 210, low: 190},
-                  {high: 200, mid: 180, low: 160}
-              ]
-          ],
-          types: {
-              data4: "area-spline-range"
-          }
-      });
-  }, 1500);
+import data from '../../datasSets/dataGPY.json';
+
+  const R = require('ramda');
+  const annees = R.uniq(data.map(d => d.date))
+  const count = annee => data.filter(d => d.date === annee).length;
+  annees.map(annee => ({ annee, count: count(annee) }))
+  console.log(annees)
