15557528f10a7bd7720aaa49d6250c5a5d1a7ec0
nobrega1 <56161445+nobrega1@users.noreply.github.com>
2020-04-24 16:37:18 +0200

    Update graphGPY.js

--- src/js/Games_per_year/graphGPY.js
+++ src/js/Games_per_year/graphGPY.js
@@ -1,7 +1,3 @@
 import data from '../../datasSets/dataGPY.json';
 
-  const R = require('ramda');
-  const annees = R.uniq(data.map(d => d.date))
-  const count = annee => data.filter(d => d.date === annee).length;
-  annees.map(annee => ({ annee, count: count(annee) }))
-  console.log(annees)
+console.log("test");
