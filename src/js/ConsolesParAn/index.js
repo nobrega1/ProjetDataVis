c71b83483e51596fb690a9b6657ef6ecf82bb0f2
nobrega1 <56161445+nobrega1@users.noreply.github.com>
2020-05-01 09:07:04 +0200

    uptade

--- src/js/ConsolesParAn/index.js
+++ src/js/ConsolesParAn/index.js
@@ -4,22 +4,15 @@ import data from '../../../data/ConsoleParAn/data.json'
 export default graphId => {
     bb.generate({
         data: {
-<<<<<<< HEAD
             columns:[
                 [data.filter(d => d.year === input).map(d => d['console']), data.filter(d => d.year === input).map(d => d.nombreDeSorties)],
             ],
-=======
-            json: {
-                ['console']: data.map(d => d['console']),
-                ['nombre sortie']: data.map(d => d['nombreDeSorties']),
-            },
->>>>>>> parent of 6066fd1... uptade
             type: "donut",  
         },
         donut: {
             title: "Console Games Releases Per Year"
           },
-        bindto: "#donutConsole"
+        bindto: document.getElementById(graphId)
         })
       }
       
