6066fd111649610b9a100f6d03bc5fe33c6f9da4
nobrega1 <56161445+nobrega1@users.noreply.github.com>
2020-05-01 08:25:11 +0200

    uptade

--- src/js/ConsolesParAn/index.js
+++ src/js/ConsolesParAn/index.js
@@ -2,18 +2,18 @@ import bb from 'billboard.js'
 import data from '../../../data/ConsoleParAn/data.json'
 
 export default graphId => {
+    const input = document.getElementById(`${graphId}-year-input`)
     bb.generate({
         data: {
-            json: {
-                ['console']: data.map(d => d['console']),
-                ['nombre sortie']: data.map(d => d['nombreDeSorties']),
-            },
+            columns:[
+                [data.filter(d => d.year === year).map(d => d['console']), data.filter(d => d.year === year).map(d => d.nombreDeSorties)],
+            ],
             type: "donut",  
         },
         donut: {
             title: "Console Games Releases Per Year"
           },
-        bindto: "#donutConsole"
+        bindto: document.getElementById(graphId)
         })
       }
       
