383b65d2dd4608ab9ca91bf2234c36ccfc4e98a4
Leonard Michiels <leonard.michiels@heig-vd.ch>
2020-05-04 21:22:48 +0200

    shenanigans

--- data/TypeJeu/prepare.js
+++ data/TypeJeu/prepare.js
@@ -7,14 +7,33 @@ const genres = R.uniq(gameType.map(d => d.Genre)).filter(genre => typeof genre =
 
 
 
-const getByYearAndGenre = (year, genre) => gameType.filter(d => d.year === year && d.Genre === genre)
-const countByYearAndGenre = (year, genre) =>  gameType.filter(d => d.year === year && d.Genre === genre).length
+/* const getByYearAndGenre = (year, genre) => gameType.filter(d => d.year === year && d.Genre === genre)
+ */const countByYearAndGenre = (year, genre) =>  gameType.filter(d => d.year === year && d.Genre === genre).length
+
+
+
+const forOneType = genre =>
+  years.map(year => ({genre, year, count: countByYearAndGenre(year,genre) }))
+
+const data = R.flatten(genres.map(forOneType))
+    .filter(d => d.count !== 0)
+    .map(genre => ({ genre, year: year.map(genre) }))
+
+    //
+
+    const findByYearAndGenre = (genre,data) => year => {
+      const dataByGenreByYear = data.find(d => d.genre === genre)
+      return {
+        annee,
+        genre: R.propOr(11, 'genre', dataByGenreByYear),
+        count: R.propOr(11, 'count', dataByGenreByYear)
+      }
+    }
+    
+    const result = dataByGenreByYear
+      .map(genre => ({ genre, data: years.map(findByYearAndGenre(genre,data)) }))
 
 
 
 
-const count = R.flatten(
-  years.map(year =>
-    genres.map(genre => ({ year, genre, count: countByYearAndGenre(year, genre) })) ) 
-)
- console.log(JSON.stringify(count))
+ console.log(JSON.stringify(data))
