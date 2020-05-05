7535fd7d00f5a49ebea3790fd43b39bae5a62695
nobrega1 <56161445+nobrega1@users.noreply.github.com>
2020-05-05 18:41:21 +0200

    Update index.js

--- src/js/ConsolesParAn/index.js
+++ src/js/ConsolesParAn/index.js
@@ -3,7 +3,7 @@ import data from '../../../data/ConsoleParAn/data.json'
 
 export default graphId => {
     const input= document.getElementById(`${graphId}-input`)
-    console.log(number(input.value))
+    console.log(input.value)
     const mesdonnes = data.filter(d => d.year === input.value).filter(d => d.nombreDeSorties != 0);
     bb.generate({
         data: {
